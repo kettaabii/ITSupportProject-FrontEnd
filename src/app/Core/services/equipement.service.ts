8f62d41171f0c259faab8f0abff90fa27bed8654
kettaabii <elkettabimohammedamin@gmail.com>
2024-08-11 00:53:01 +0100

    added equipement service and panne service

--- src/app/Core/services/equipement.service.ts
+++ src/app/Core/services/equipement.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Equipement} from "../models/equipement";
 
 @Injectable({
   providedIn: 'root'
 })
 export class EquipementService {
+  private baseUrl = 'http://localhost:8088';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  addNewEquipement(equipement: Equipement): Observable<Equipement> {
+    return this.http.post<Equipement>(`${this.baseUrl}/admin/AddNewEquipement`, equipement);
+  }
+
+  updateEquipement(id: number, equipement: Equipement): Observable<Equipement> {
+    return this.http.put<Equipement>(`${this.baseUrl}/admin/updateEquipement/${id}`, equipement);
+  }
+
+  deleteEquipement(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/admin/deleteEquipement/${id}`);
+  }
+
+  getAllEquipements(): Observable<Equipement[]> {
+    return this.http.get<Equipement[]>(`${this.baseUrl}/admin/listEquipements`);
+  }
+
+  assignEquipementToUser(idMat: number, idUser: number): Observable<any> {
+    return this.http.put(`${this.baseUrl}/admin/AssignEquipement${idMat}/ToUser/${idUser}`, {});
+  }
+
+  getEquipementPerUser(id: number): Observable<Equipement[]> {
+    return this.http.get<Equipement[]>(`${this.baseUrl}/admin/equipementPerUser/${id}`);
+  }
+
+  getEquipmentById(id: number): Observable<Equipement> {
+    return this.http.get<Equipement>(`${this.baseUrl}/admin/getEquipmentById/${id}`);
+  }
 }
--- src/app/Core/services/panne.service.ts
+++ src/app/Core/services/panne.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Panne} from "../models/panne";
+import { PanneEquipementKey} from "../models/panne-equipement-key";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PanneService {
+ private baseUrl='http://localhost:8088'
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  signaler(description: string, idMat: number, idPanne: number, idUser: number): Observable<string> {
+   const params ={description,idMat,idPanne}
+    return this.http.post(`${this.baseUrl}/user/signaler/${idUser}`,null,{params,responseType:'text'});
+  }
+
+  newPanne(panne:Panne) : Observable<Panne> {
+   return this.http.post<Panne>(`${this.baseUrl}/admin/newPanne`,panne);
+  }
+
+  setAsCommunePanne(panne:PanneEquipementKey):Observable<Panne>{
+   return this.http.post<Panne>(`${this.baseUrl}/admin/setAsCommunePanne`,panne);
+  }
+
+  updatePanne (id:number,panne:Panne):Observable<string> {
+   return this.http.put(`${this.baseUrl}/admin/updatePanne/${id}`,panne , {responseType:'text'});
+  }
+
+  getAllPannes() :Observable<Panne[]>{
+   return this.http.get<Panne[]>(`${this.baseUrl}/admin/allPanne`);
+  }
+
 }
