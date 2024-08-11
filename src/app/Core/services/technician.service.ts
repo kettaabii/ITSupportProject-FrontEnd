8f3201f45dd10c4e681c785753b615f8b9f6aa62
kettaabii <elkettabimohammedamin@gmail.com>
2024-08-11 01:25:45 +0100

    added technician service and ticket service

--- src/app/Core/services/technician.service.ts
+++ src/app/Core/services/technician.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {SignupRequest} from "../dtos/signup-request.dto";
+import {Technician} from "../models/technician";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TechnicianService {
+  private baseUrl ='http://localhost:8088'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  newTechnician(technician:SignupRequest) : Observable<string>{
+    return this.http.post(`${this.baseUrl}/admin/newTechnician`,technician,{responseType:'text'});
+  }
+
+  updateTechnician(id:number ,technician:SignupRequest): Observable<Technician>{
+    return this.http.put(`${this.baseUrl}/admin/updateTechnician/${id}`,technician);
+  }
+
+  deleteTechnician(id:number):Observable<string>{
+    return this.http.delete(`${this.baseUrl}/admin/deleteTechnician/${id}`, {responseType:'text'});
+  }
 }
--- src/app/Core/services/ticket.service.ts
+++ src/app/Core/services/ticket.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Ticket} from "../models/ticket";
+import {TicketHistoryDto} from "../dtos/ticket-history-dto.dto";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketService {
+  private baseUrl='http://localhost:8088';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  assignTicketToTechnician(idTicket:number ,idTechnician:number):Observable<void>{
+    return this.http.put<void>(`${this.baseUrl}/admin/AssignTicket/${idTicket}/toTechnician/${idTechnician}`,null);
+  }
+
+  getTicketsByTec(id:number):Observable<Ticket[]>{
+    return this.http.get<Ticket[]>(`${this.baseUrl}/technician/getTicketsByTec/${id}`);
+  }
+
+  getAssignedTickets(id:number):Observable<Ticket[]>{
+    return this.http.get<Ticket[]>(`${this.baseUrl}/technician/AssignedTickets/${id}`);
+  }
+
+  getTicketHistoryForMat(id:number):Observable<TicketHistoryDto[]>{
+    return this.http.get<TicketHistoryDto[]>(`${this.baseUrl}/admin/TicketsHistoryForMat/${id}`);
+  }
+
+  acceptTicket(id:number):Observable<string>{
+    return this.http.put(`${this.baseUrl}/technician/AcceptTicket/${id}`,null,{responseType:'text'});
+  }
+
+  getMyTickets(id:number):Observable<TicketHistoryDto[]>{
+    return this.http.get<TicketHistoryDto[]>(`${this.baseUrl}/user/MyTickets/${id}`);
+  }
 }
